afa5f14dc0cf140472be37e76e2389950a075ad5
tomlowen <tom.r.lowen@googlemail.com>
2021-12-15 09:53:38 +0000

    request class added

--- core/Router.php
+++ core/Router.php
@@ -5,8 +5,20 @@ namespace app\core;
 class Router
 {
     protected array $routes = [];
+    public Request $request;
+
+    public function __construct($request)
+    {
+        $this->request = $request;
+    }
+
     public function get($path, $callback)
     {
         $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $path = $this->request->getPath();
     }
 }
--- index.php
+++ index.php
@@ -5,9 +5,12 @@ require_once __DIR__ . "/vendor/autoload.php";
 use app\core\Application;
 
 $app = new Application();
+
 $app->router->get("/", function () {
     return "hello world";
 });
 $app->router->get("/contact", function () {
     return "you have reached contacts";
 });
+
+$app->run();

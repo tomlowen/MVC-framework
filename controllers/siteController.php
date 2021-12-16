ec9981cfcc278e131200f1ab28958f25cef9ecfe
tomlowen <tom.r.lowen@googlemail.com>
2021-12-16 10:38:31 +0000

    controller hanndling views; need to resolve 500 error

--- controllers/siteController.php
+++ controllers/siteController.php
@@ -2,13 +2,22 @@
 
 namespace app\controllers;
 
-class SiteController
+use app\core\Controller;
+class SiteController extends Controller
 {
     public function handleContact()
     {
         return 'Handling Submitted Data';
     }
+    public function home()
+    {
+        $params = [
+            'name' => 'Tom'
+        ];
+        return $this->render('home', $params);
+    }
     public function contact()
     {
+        return $this->render('contact');
     }
 }
--- core/Router.php
+++ core/Router.php
@@ -18,7 +18,7 @@ class Router
     {
         $this->routes['get'][$path] = $callback;
     }
-    
+
     public function post($path, $callback)
     {
         $this->routes['post'][$path] = $callback;
@@ -31,15 +31,18 @@ class Router
         return ob_get_clean();
     }
 
-    public function renderView($view)
+    public function renderView($view, $params = [])
     {
         $layoutContent = $this->layoutContent();
-        $viewContent = $this->renderOnlyView($view);
+        $viewContent = $this->renderOnlyView($view, $params);
         return str_replace('{{content}}', $viewContent, $layoutContent);
     }
 
-    protected function renderOnlyView($view)
+    protected function renderOnlyView($view, $params)
     {
+        foreach ($params as $key => $value) {
+            $$key = $value;
+        }
         ob_start();
         include_once Application::$ROOT_DIR . "/views/$view.php";
         return ob_get_clean();
